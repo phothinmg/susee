4eb0fb3b3e06428b1937272004ef3be6280976fd
phothinmg <phothinmg@disroot.org>
2026-08-01 00:25:00 +0630

    🎨 modified : docs mmcov

--- susee-docs/scripts/dev.sh
+++ susee-docs/scripts/dev.sh
@@ -4,7 +4,4 @@ set -euo pipefail
 # jekyll build
 JEKYLL_ENV=production bundle exec jekyll build
 sleep 0.5
-# mmcov build
-bash scripts/mmcov.sh
-sleep 1
 node scripts/dev_server.mjs
--- susee-docs/scripts/vercel_build.sh
+++ susee-docs/scripts/vercel_build.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# coverage
-# to susee root
-cd ..
-echo "Running coverage"
-npx tsx --test --experimental-test-coverage --test-reporter=lcov --test-reporter-destination=__tests__/coverage/lcov.info
-cd susee-docs
-echo "Done running coverage"
 # pre cache
 mkdir -p docs/.jekyll-cache && cp -r node_modules/.jekyll-cache-backup/. docs/.jekyll-cache/ 2>/dev/null || true 
 sleep 0.5
 # jekyll build
 JEKYLL_ENV=production bundle exec jekyll build
 sleep 0.5
-# mmcov build
-bash scripts/mmcov.sh
-sleep 1
 # post build
 mkdir -p node_modules/.jekyll-cache-backup && cp -r docs/.jekyll-cache/. node_modules/.jekyll-cache-backup/
