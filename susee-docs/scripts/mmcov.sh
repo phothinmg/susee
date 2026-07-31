711a3e0f4c27e9a57884de94820dce892e789fbe
phothinmg <phothinmg@disroot.org>
2026-07-31 17:10:37 +0630

    📝 docs : edited

--- susee-docs/scripts/mmcov.sh
+++ susee-docs/scripts/mmcov.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 cd ..
-# to susee
+sleep 0.5
+npx tsx --test --experimental-test-coverage --test-reporter=lcov --test-reporter-destination=__tests__/coverage/lcov.info
+sleep 1
 npx mmcov __tests__/coverage/lcov.info --out susee-docs/_site/coverage --project susee --source src --favicon susee-docs/public/favicons/favicon.ico --mmdocs
 sleep 1
 cd susee-docs
