7a3cf92f4a19367a78bc0b2ffbfd3151a6a5b939
phothinmg <phothinmg@disroot.org>
2026-08-30 00:28:28 +0630

    🎨 modified : wf

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,15 +4,14 @@ set -euo pipefail
 repo_root="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$repo_root"
 
-npx napi build --platform --config-path napi.config.json --output-dir ./dist
-
+npx napi build --platform --config-path napi.config.json
 mkdir -p bin
 
 cat <<EOF > bin/susee
 #!/usr/bin/env node
 
-var {cliBuild} = require("../dist/index.js")
+var {cliBuild} = require("../index.js")
 cliBuild(process.argv.slice(2));
 EOF
 
-rm -rf dist/susee.linux-x64-gnu.node
+rm -rf susee.linux-x64-gnu.node
