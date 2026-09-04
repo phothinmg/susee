98f5d660fe6bf2c1b7f61e862a9d5ec522960c33
phothinmg <phothinmg@disroot.org>
2026-09-04 18:21:41 +0630

    🎨 modified : wf

--- build.ts
+++ build.ts
@@ -1,16 +1,31 @@
 import { build } from "./src/index.js";
+import path from "node:path";
+import fs from "node:fs";
+
+async function writeBinary() {
+  const binaryText = `#!/usr/bin/env node
+import "../dist/cli/index.mjs";`;
+  const binaryPath = path.resolve(process.cwd(), "bin/susee");
+  const parentDir = path.dirname(binaryPath);
+  if (fs.existsSync(binaryPath)) await fs.promises.unlink(binaryPath);
+  if (!fs.existsSync(parentDir)) await fs.promises.mkdir(parentDir);
+  await fs.promises.writeFile(binaryPath, binaryText);
+  fs.promises.chmod(binaryPath, 0o777);
+}
 
 await build({
-    entryPoints:[
-        {
-            entry:"src/index.ts",
-            format:["commonjs","esm"],
-            exportPath:".",
-        },
-        {
-            entry:"src/cli/index.ts",
-            exportPath:"./cli",
-        }
-    ],
-    allowUpdatePackageJson: true
-})
+  entryPoints: [
+    {
+      entry: "src/index.ts",
+      format: ["commonjs", "esm"],
+      exportPath: ".",
+    },
+    {
+      entry: "src/cli/index.ts",
+      exportPath: "./cli",
+    },
+  ],
+  allowUpdatePackageJson: true,
+});
+
+await writeBinary();
